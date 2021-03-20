ca076844ae337f963af6ee29d16b20dd5f73d303
Jen <tryphenayen@gmail.com>
2021-03-21 00:38:00 +0800

    加入專案檔案。

---
+++ SignalR_Testing/Scripts/SignalR.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    $.connection.hub.start()
+        .done(function () {
+            console.log("it worked!");
+            console.log($.connection.myHub)
+            $.connection.myHub.server.announce("connected");
+        })
+        .fail(function () { alert("error"); });
+
+    $.connection.myHub.client.announce = function (message) {
+        $("#welcome-messages").append(message);
+    };
+})()
---
+++ SignalR_Testing/myHub.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalR_Testing
+{
+    public class myHub : Hub
+    {
+        //public void Hello()
+        //{
+        //    Clients.All.hello();
+        //}
+        //public void Send(string name, string message)
+        //{
+        //    // Call the broadcastMessage method to update clients.
+        //    Clients.All.broadcastMessage(name, message);
+        //}
+
+        public void Announce(string message)
+        {
+            Clients.All.Announce(message);
+        }
+    }
+}
